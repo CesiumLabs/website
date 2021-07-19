98412002e8f5e54b63fe676fa0c0543455ee312d
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-07-19 16:28:08 +0545

    fix: build error

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
-    future: {
-        webpack5: true,
-    },
+    webpack5: true,
     pwa: {
         dest: "public",
         runtimeCaching,
